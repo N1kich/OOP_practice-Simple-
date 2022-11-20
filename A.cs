7bc96b848a2c6195edf086f30a4d304d811ae84b
N1kich <funn1k.pro@gmail.com>
2022-11-20 17:24:12 +0300

    First task completed

---
+++ A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_practice
+{
+    public class A
+    {
+        public void M()
+        {
+            Console.WriteLine("Method by A class " + this.GetType().ToString());
+        }
+        public void C()
+        {
+
+        }
+    }
+
+    public class B : A
+    {
+        public new void M()
+        {
+            Console.WriteLine("Method from B class " + this.GetType().ToString());
+        }
+    }
+}
---
+++ Source/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_practice.Source
+{
+    internal class Client
+    {
+        string _name;
+        public string Name { get { return _name; } }
+
+        string _surname;
+        public string Surname { get { return _surname; } }
+
+        string _lastName;
+        public string LastName { get { return _lastName; } }
+
+        string _passportData;
+
+        char[] separators;
+        public string PassportData {
+            get
+            {
+                string[] splittedPassport = _passportData.Split(separators);
+                return String.Join("*", splittedPassport);
+            }
+        }
+
+        string _phoneNumber;
+        public string PhoneNumber { 
+            get { return _phoneNumber; }
+            set 
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("Phone number can't be null");
+                }
+                else
+                {
+                    _phoneNumber = value;
+                }
+            }
+        }
+
+        public Client(string name, string surname, string lastName, string passportData,
+            string phoneNumber)
+        {
+            _name = name;
+            _surname = surname;
+            _lastName = lastName;
+            _passportData = passportData;
+            _phoneNumber = phoneNumber;
+
+            separators = new char[]  { '0','1','2','3', '4','5', '6', '7', '8', '9', };
+        }
+    }
+}
